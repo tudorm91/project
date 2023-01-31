1fb6739a74f5df831772f3ccd01d153c15f177f9
tudorm91 <tudor.munteanu@endava.com>
2023-01-31 10:13:17 +0200

    feat/task1-add-Two-Numbers

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("start");
+        int num1;
+        double num2, sum;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("First number : ");
+        num1 = sc.nextInt();
+        System.out.println("Second number : ");
+        num2 = sc.nextDouble();
+        sum = addTwoNumbers(num1, num2);
+        System.out.println("Sum : " + sum);
+        sc.close();
     }
-}
+
+
+    public static double addTwoNumbers (int term1, double term2)
+    {
+        return term1 + term2;
+
+    }}
