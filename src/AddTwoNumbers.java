901b4a59490e7e561e1cb2d43f4a475167562a49
Tudor Munteanu <69298588+tudorm91@users.noreply.github.com>
2023-02-01 11:42:06 +0200

    feat/task1-add-Two-Numbers (#2)
    * feat/task1-add-Two-Numbers
    
    * feat/task1-add-Two-Numbers
    
    * feat/task1-add-Two-Numbers
    
    * feat/task1-add-Two-Numbers
    
    ---------
    
    Co-authored-by: tudorm91 <tudor.munteanu@endava.com>

---
+++ src/AddTwoNumbers.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class AddTwoNumbers {
+    public static void main(String[] args) {
+        int firstNumber;
+        double secondNumber, sum;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("First number : ");
+        firstNumber = sc.nextInt();
+        System.out.println("Second number : ");
+        secondNumber = sc.nextDouble();
+        sum = addTwoNumbers(firstNumber, secondNumber);
+        System.out.println("Sum : " + sum);
+        sc.close();
+    }
+
+    public static double addTwoNumbers(int firstNumber, double secondNumber) {
+        return  firstNumber + secondNumber;
+    }
+}
