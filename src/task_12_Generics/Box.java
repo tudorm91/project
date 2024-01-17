5304a587fb555d702ad6ab19c5b015c951227ae0
tudorm91 <tudor.munteanu@endava.com>
2024-01-17 15:59:58 +0200

    task_11_Abstractisation

---
+++ src/task_12_Generics/Box.java
@@ -0,0 +1,13 @@
+package task_12_Generics;
+
+public class Box<T> {
+    private T value;
+
+    public Box(T value) {
+        this.value = value;
+    }
+
+    public T getValue() {
+        return value;
+    }
+}
